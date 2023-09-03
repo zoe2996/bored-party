e34419318ee80072d0a12e78eebef694c10bd6ae
Alonzo Manaloto <zoe.manaloto@gmail.com>
2023-09-04 00:34:50 +0800

    refined list

--- src/app/netflix/decks/deckCollection.tsx
+++ src/app/netflix/decks/deckCollection.tsx
@@ -1,12 +1,16 @@
 import { NetflixAttribute } from "../types/netflix.type";
-import { DISNEY_CARDS } from "./disney";
-import { ENGLISH_CARDS } from "./english";
+import { ACTION_CARDS, CARTOON_CARDS, COMEDY_CARDS, CRIME_SUSPENSE_CARDS, ENGLISH_CARDS, HORROR_CARDS, SCIFI_CARDS } from "./english";
 import { PINOY_CARDS } from "./pinoy";
 
 export const DECKS: { [deckName: string]: Array<NetflixAttribute> } = {
   pinoy: PINOY_CARDS,
   english: ENGLISH_CARDS,
-  disney: DISNEY_CARDS
+  cartoon: CARTOON_CARDS,
+  scifi:SCIFI_CARDS,
+  action: ACTION_CARDS,
+  comedy: COMEDY_CARDS,
+  horror: HORROR_CARDS,
+  crime_suspense: CRIME_SUSPENSE_CARDS
 };
 
 export function getAllDecks() {
