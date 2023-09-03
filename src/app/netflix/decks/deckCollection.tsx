4058d74dfb51dbac2b35fbc291c42c29cc53cd8e
Alonzo Manaloto <zoe.manaloto@gmail.com>
2023-09-04 00:48:17 +0800

    added anime decks

--- src/app/netflix/decks/deckCollection.tsx
+++ src/app/netflix/decks/deckCollection.tsx
@@ -1,22 +1,36 @@
 import { NetflixAttribute } from "../types/netflix.type";
-import { ACTION_CARDS, CARTOON_CARDS, COMEDY_CARDS, CRIME_SUSPENSE_CARDS, ENGLISH_CARDS, HORROR_CARDS, SCIFI_CARDS } from "./english";
+import { ANIME_CARDS } from "./anime";
+import {
+  ACTION_CARDS,
+  CARTOON_CARDS,
+  COMEDY_CARDS,
+  CRIME_SUSPENSE_CARDS,
+  ENGLISH_CARDS,
+  HORROR_CARDS,
+  SCIFI_CARDS,
+} from "./english";
 import { PINOY_CARDS } from "./pinoy";
 
-export const DECKS: { [deckName: string]: Array<NetflixAttribute> } = {
+const NORMAL_DECKS: { [deckName: string]: Array<NetflixAttribute> } = {
   pinoy: PINOY_CARDS,
   english: ENGLISH_CARDS,
   cartoon: CARTOON_CARDS,
-  scifi:SCIFI_CARDS,
+  scifi: SCIFI_CARDS,
   action: ACTION_CARDS,
   comedy: COMEDY_CARDS,
   horror: HORROR_CARDS,
-  crime_suspense: CRIME_SUSPENSE_CARDS
+  crime_suspense: CRIME_SUSPENSE_CARDS,
 };
 
+const SECRET_CATEGORIES: { [deckName: string]: Array<NetflixAttribute> } = {
+  anime: ANIME_CARDS,
+};
+
+export const DECKS = { ...NORMAL_DECKS, ...SECRET_CATEGORIES };
 export function getAllDecks() {
   let decks: Array<NetflixAttribute> = [];
-  Object.keys(DECKS).forEach((deckName) => {
-    decks = decks.concat(DECKS[deckName]);
+  Object.keys(NORMAL_DECKS).forEach((deckName) => {
+    decks = decks.concat(NORMAL_DECKS[deckName]);
   });
   return decks;
 }
