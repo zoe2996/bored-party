2fd2f525304995ee28087ea984a398e7df4803ff
Alonzo Manaloto <zoe.manaloto@gmail.com>
2023-09-03 20:13:02 +0800

    fixed error

--- src/app/netflix/[deck]/page.tsx
+++ src/app/netflix/[deck]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { TabooPage } from "../components";
+import { NetflixPage } from "../components";
 
 export default function Deck() {
   const params = useParams()
-  return <TabooPage deckName={params.deck.toString()}></TabooPage>;
+  return <NetflixPage deckName={params.deck.toString()}></NetflixPage>;
 }
