d9e5d20f4505c3612acc2e9ea3b7499c6c92e98c
Alonzo Manaloto <zoe.manaloto@gmail.com>
2023-09-03 20:22:22 +0800

    Added disney

--- src/app/netflix/decks/deckCollection.tsx
+++ src/app/netflix/decks/deckCollection.tsx
@@ -1,10 +1,12 @@
 import { NetflixAttribute } from "../types/netflix.type";
+import { DISNEY_CARDS } from "./disney";
 import { ENGLISH_CARDS } from "./english";
 import { PINOY_CARDS } from "./pinoy";
 
 export const DECKS: { [deckName: string]: Array<NetflixAttribute> } = {
   pinoy: PINOY_CARDS,
   english: ENGLISH_CARDS,
+  disney: DISNEY_CARDS
 };
 
 export function getAllDecks() {
