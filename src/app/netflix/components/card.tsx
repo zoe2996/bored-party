72f39184a02e122f1457aba7692769a5aa79600d
zoe2996 <zoe.manaloto@gmail.com>
2023-09-05 00:16:56 +0800

    added home page and font adjustment

--- src/app/netflix/components/card.tsx
+++ src/app/netflix/components/card.tsx
@@ -11,16 +11,33 @@ export function NetflixCard({
   netflixAttribute: NetflixAttribute;
   category: string;
 }) {
+  function getTextSizeClass(answer: string): string {
+    if (answer.length <= 20) {
+      return "text-3xl";
+    } else if (answer.length <= 40) {
+      return "text-2xl";
+    } else {
+      return "text-xl";
+    }
+  }
+
   const innerContent: ReactElement = (
     <>
       <div className="p-5 text-center w-full h-full bg-black flex items-center flex-col">
-        <Image src="/netflix-logo.webp" alt={""} width={45} height={45} />
-        <div className="px-3 text-center m-auto">
-          <h1 className="font-sans font-bold text-4xl text-white text-center">
-            {netflixAttribute.answer}
-          </h1>
+        <div className="px-3 text-center m-auto flex-col flex h-full items-center">
+          <Image src="/netflix-logo.webp" alt={""} width={45} height={45} />
+          <div className="h-2/3 overflow-hidden flex flex-col align-middle justify-center items-center">
+            <h1
+              className={
+                "font-sans font-bold text-white text-center " +
+                getTextSizeClass(netflixAttribute.answer)
+              }
+            >
+              {netflixAttribute.answer}
+            </h1>
+          </div>
 
-          <div className="text-white text-2xl mb-3">
+          <div className="text-white text-2xl pb-3 h-1/3">
             <hr className="my-3"></hr>
             <h4>{category}</h4>
           </div>
