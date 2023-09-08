f7314e50cf8affc8f70dd515629ff913441f4ece
Alonzo Manaloto <alonzo@elephantventures.com>
2023-09-09 01:21:28 +0800

    fixed live mode for impersonator

--- src/pages/api/impersonator.ts
+++ src/pages/api/impersonator.ts
@@ -1,13 +1,13 @@
-import { NetflixAttribute } from "@/app/netflix/types/netflix.type";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getColumnName, getContents } from "./shared/sheets";
+import { ImpersonatorAttribute } from "@/app/impersonator/types/impersonator.type";
 
 type ResponseData = {
-  netflixAttributes?: Array<NetflixAttribute>;
+  impersonatorAttributes?: Array<ImpersonatorAttribute>;
   message?: string;
 };
 const SHEET_NAME = "impersonator";
-export default async function getNetflixSheetAttributes(
+export default async function getImpersonatorAttributes(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
@@ -16,26 +16,28 @@ export default async function getNetflixSheetAttributes(
   const categoryIndexNumber = getIndexNumber(category, categories ?? []);
   const columnName = getColumnName(categoryIndexNumber);
 
-  const data = await getContents(`${SHEET_NAME}!${columnName}2:${columnName}1000`);
+  const data = await getContents(
+    `${SHEET_NAME}!${columnName}2:${columnName}1000`
+  );
 
   if (data !== null) {
     const response = await buildAttributes(data);
-    res.status(200).json({ netflixAttributes: response });
+    res.status(200).json({ impersonatorAttributes: response });
   } else {
     res.status(400).json({ message: "ERROR OCCURRED" });
   }
 }
 
-function buildAttributes(data: Array<any>): Array<NetflixAttribute> {
-  let netflixAttributes: Array<NetflixAttribute> = [];
+function buildAttributes(data: Array<any>): Array<ImpersonatorAttribute> {
+  let impersonatorAttributes: Array<ImpersonatorAttribute> = [];
   data.forEach((row: Array<string>) => {
     if (row[0]) {
       if (row[0].trim() !== "") {
-        netflixAttributes.push({ answer: row[0] });
+        impersonatorAttributes.push({ answer: row[0] });
       }
     }
   });
-  return netflixAttributes;
+  return impersonatorAttributes;
 }
 
 function getIndexNumber(category: string, rows: Array<Array<string>>) {
