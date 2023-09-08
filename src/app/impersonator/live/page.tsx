e90ab55683c9f40ce5e079b191596e4eea70b758
Alonzo Manaloto <alonzo@elephantventures.com>
2023-09-09 01:25:47 +0800

    added metadata

--- src/app/impersonator/live/page.tsx
+++ src/app/impersonator/live/page.tsx
@@ -6,8 +6,14 @@ import { ImpersonatorPage } from "../components/main";
 export default function Deck() {
   const params = useParams();
   return (
-    <ImpersonatorPage
-      live={true}
-    ></ImpersonatorPage>
+    <>
+      <title>Impersonator</title>
+      <meta
+        name="og:description"
+        content="Make them guess the personality by their Famous Lines, Behaviour or just Posing It!"
+      ></meta>
+      <meta name="og:image" content="https://imgur.com/a/kJfS1ob"></meta>
+      <ImpersonatorPage live={true}></ImpersonatorPage>
+    </>
   );
 }
